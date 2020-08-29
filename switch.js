f3f45f3affabe797735134741cf50e44b8a619e3
Jose Villarroel <josedelvallevs@gmail.com>
2020-08-28 23:54:58 -0500

    Lección 13: Switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+switch (true) {
+    case 1:
+        console.log("Soy uno!");
+        break; 
+    case 10:
+        console.log("Soy un 10!");
+        break;
+    case 100; 
+        console.log("Soy un 100!"); 
+        break; 
+    default:
+        console.log("NO soy nada!");
+        
+}
