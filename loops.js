19eaf912dc1036a1ce0fae24b464acbdbd30ffd9
Jose Villarroel <josedelvallevs@gmail.com>
2020-08-30 23:37:49 -0500

    Lección 16: Loops:While

--- loops.js
+++ loops.js
@@ -13,5 +13,17 @@ for(var estudiante of estudiantes){
     saludarEstudiantes(estudiante);
 }
 
+/*  Practica en la consola
+var estudiantes = ["Maria","Sergio","Rosa","Daniel"];
 
+function saludarEstudiantes(estudiante) {
+    console.log(`Hola, ${estudiante}`);
+}
+while(estudiantes.length > 0){
+    console.log(estudiantes);
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
+
+*/
 
